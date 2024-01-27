895ad68dfc4efe658eca8c476f4dd1e5057a4a9c
Jerry <yg_choi@caredoc.kr>
2024-01-27 18:17:39 +0900

    파일 업로드 모듈 공통화

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,4 +2,4 @@ export * from './common.module';
 export * from './common.service';
 export * from './enum';
 export * from './util';
-export * from './multer';
+export * from './upload/multer';
--- libs/common/src/upload/upload.module.ts
+++ libs/common/src/upload/upload.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
-import { multerOptionsFactory } from '@app/common/multer';
+import { multerOptionsFactory } from '@app/common/upload/multer';
+import { UploadController as ProductUploadController } from 'apps/mymovester-api/src/upload/upload.controller';
+import { UploadController as BackofficeUploadController } from 'apps/backoffice-api/src/upload/upload.controller';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { multerOptionsFactory } from '@app/common/multer';
       inject: [ConfigService],
     }),
   ],
-  controllers: [UploadController],
+  controllers: [ProductUploadController, BackofficeUploadController],
   providers: [UploadService],
 })
 export class UploadModule {}
