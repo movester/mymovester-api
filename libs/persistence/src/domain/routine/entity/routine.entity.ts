0ffff818ad791564a91af6eb9d49b8d8c8d17b7c
Jerry <yg_choi@caredoc.kr>
2024-03-13 20:36:21 +0900

    ADD: routine entity

---
+++ libs/persistence/src/domain/routine/entity/routine.entity.ts
@@ -0,0 +1,55 @@
+import { BaseEntityClass } from "@app/persistence/domain/base-entity.entity";
+import { RoutineItem } from "@app/persistence/domain/routine/entity/routine-item.entity";
+import { User } from "@app/persistence/domain/user/entity/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne } from "typeorm";
+
+
+@Entity('routine')
+export class Routine extends BaseEntityClass {
+
+  @Column({name: 'title',
+    type: 'varchar',
+    length: 255,
+    default: '',comment: '제목',
+  })
+  title: string;
+
+  @Column({
+    name: 'order',
+    type: 'int',
+    default: 1,
+    comment: '루틴 폴더 순서',
+  })
+  order: number;
+
+  @Column({
+    name: 'user_id',
+    type: 'int',
+    comment: '회원ID',
+  })
+  userId: number;
+
+  @OneToOne(
+    () => User,
+    (user) => user,
+    {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      cascade: ['insert'],
+      nullable: true,
+    },
+  )
+  user: User;
+
+  @OneToMany(
+    () => RoutineItem,
+    (routineItem) => routineItem.routine,
+    {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      cascade: ['insert'],
+      nullable: true,
+    },
+  )
+  routineItems: RoutineItem[];
+}
