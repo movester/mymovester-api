c776c6dab1f41a2fdbcefdc21e1a9e0d8f0d34b5
jnhro1 <jnhro1@gmail.com>
2024-03-15 18:41:57 +0900

    - 스트레칭 상세 조회시 좋아요 여부를 표시한다. - 로그인, 비로그인 모두 접근할 수는 accessAuthGaurd 를 추가한다 (리팩토링 예정)

--- apps/mymovester-api/src/stretching/stretching.controller.ts
+++ apps/mymovester-api/src/stretching/stretching.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { StretchingDetailResponse } from './response/stretching-detail.response';
 import { StretchingService } from './stretching.service';
 import { StretchingListResponse } from './response/stretching-list.response';
 import { GetStretchingListRequest } from './request/get-stretching-list.request';
+import { AccessAuthGuard } from '../auth/access-auth.guard';
+import { UserDeco } from '../shared/decorator/user.decorator';
+import { IUser } from '../user/user.interface';
 
 @Controller('stretchings')
 export class StretchingController {
   constructor(private stretchingService: StretchingService) {}
 
   @Get('/:id')
+  @UseGuards(AccessAuthGuard)
   getStretchingById(
     @Param('id', ParseIntPipe) id: number,
+    @UserDeco() user: IUser,
   ): Promise<StretchingDetailResponse> {
-    return this.stretchingService.getStretchingById(id);
+    return this.stretchingService.getStretchingById(id, user.id);
   }
 
   @Get('/')
