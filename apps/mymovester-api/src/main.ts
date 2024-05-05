8493c8200ba08ec891a99e86cb458e5c71a7164b
hiYong94 <40785404+hiYong94@users.noreply.github.com>
2024-05-05 18:31:15 +0900

    swagger 실행 설정
    - swagger 실행 가능하도록 설정 (http://localhost:4000/swagger)
    - app에 사용된 모듈 주석 추가

--- apps/mymovester-api/src/main.ts
+++ apps/mymovester-api/src/main.ts
@@ -5,25 +5,29 @@ import { MymovesterModule } from './mymovester.module';
 import { HttpExceptionFilter } from '@app/common/exception/http-exception.filter';
 import { WebhookInterceptor } from '@app/common/webhook.interceptor';
 import { SentryInterceptor } from '@app/common/sentry.interceptor';
+import { swaggerBuilder } from './config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(MymovesterModule);
+
+  // API versioning
   app.setGlobalPrefix('/api/');
   app.enableVersioning({
     type: VersioningType.URI,
     prefix: 'v',
     defaultVersion: '1',
   });
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  swaggerBuilder(app); // Inject Swagger
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // DTO class-validator
+  app.useGlobalFilters(new HttpExceptionFilter()); // HTTP exception config
+  app.useGlobalInterceptors(new SentryInterceptor()); // Sentry Config
+  app.useGlobalInterceptors(new WebhookInterceptor()); // Slack Webhook Config
 
   const port = process.env.MYMOVESTER_PORT;
   Sentry.init({
     dsn: process.env.SENTRY_DSN_MYMOVESTER,
   });
-
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new SentryInterceptor());
-  app.useGlobalInterceptors(new WebhookInterceptor());
 
   await app.listen(port);
   Logger.log(`🚀mymovester-api running on port ${port}`);
