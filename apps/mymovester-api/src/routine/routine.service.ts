3016c78b7581e4329f624d298ca7cc31612d5474
hiYong94 <40785404+hiYong94@users.noreply.github.com>
2024-05-05 19:41:32 +0900

    린트 설정 추가

--- apps/mymovester-api/src/routine/routine.service.ts
+++ apps/mymovester-api/src/routine/routine.service.ts
@@ -1,15 +1,15 @@
+import { isArrayEqual } from '@app/common';
+import { RoutineRepository } from '@app/persistence/domain/routine/repository/routine.repository';
 import {
   BadRequestException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { RoutineRepository } from '@app/persistence/domain/routine/repository/routine.repository';
 import {
   GetRoutineListResponse,
   GetRoutineStretchingListResponse,
 } from 'apps/mymovester-api/src/routine/routine-response';
-import { isArrayEqual } from '@app/common';
 import { DeleteRoutinesRequest, UpdateRoutineRequest } from './routine.request';
 
 @Injectable()
